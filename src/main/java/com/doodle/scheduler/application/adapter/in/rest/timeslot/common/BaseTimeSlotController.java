e60dcb9f6914771124a39526a4b0cc606809f034
Germán Francisco Martínez Navarro <german.f.martinez@gmail.com>
2026-02-08 00:22:13 +0100

    refactor(controller): remove unused RestController annotation from BaseTimeSlotController

--- src/main/java/com/doodle/scheduler/application/adapter/in/rest/timeslot/common/BaseTimeSlotController.java
+++ src/main/java/com/doodle/scheduler/application/adapter/in/rest/timeslot/common/BaseTimeSlotController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Base controller for all time slot-related endpoints.
